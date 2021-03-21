a166f597d2395ceca7ec7eb0dcd22c6316611264
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2021-03-21 20:09:44 +0100

    .

---
+++ db-interface.js
@@ -0,0 +1,44 @@
+const mysql = require("mysql")
+
+var pool = mysql.createPool({
+    host: "192.168.2.35",
+    user: "crypto",
+    password: "CryptoStonks",
+    database: "portfoliotracker"
+})
+
+//Handles the insertion of historical data into the database
+function InsertHistorical(marketTick, cb) {
+    pool.query(`
+        INSERT INTO HistoricalData
+        VAlUES ('${marketTick.time}', '${marketTick.name}', '${marketTick.value}')`,
+        (err, res) => {
+            if (err) throw err;
+
+            if (typeof cb === "function")
+                cb(marketTick)
+        })
+}
+
+function RetrieveHistorical(market, cb) {
+    let hd = { market: market }
+
+    pool.query(`
+        SELECT DataTime, Value
+        FROM HistoricalData
+        WHERE MarketID = ${market}
+        `,
+        (err, res) => {
+            if (err) throw err;
+
+            console.log(res)
+
+            if (typeof cb === "function")
+                cb(marketTick)
+        })
+}
+
+module.exports = {
+    InsertHistorical: InsertHistorical,
+    RetrieveHistorical: RetrieveHistorical
+}
--- server.js
+++ server.js
@@ -1,31 +1,13 @@
 const bi = require('./bitvavo-interface')
 const schedule = require("node-schedule")
-const mysql = require("mysql")
-
-var pool = mysql.createPool({
-    host: "192.168.2.35",
-    user: "crypto",
-    password: "CryptoStonks",
-    database: "portfoliotracker"
-})
+const dbi = require('./db-interface')
 
 const CryptoJob = schedule.scheduleJob("*/15 * * * * *", UpdateCrypto)
 
 function UpdateCrypto() {
     bi.GetTick((data) => {
-        data.forEach(marketTick => { InsertHD(marketTick) })
+        data.forEach(marketTick => { dbi.InsertHistorical(marketTick) })
     })
 }
 
-//Handles the insertion of historical data into the database
-function InsertHD(marketTick, cb) {
-    pool.query(`
-        INSERT INTO HistoricalData
-        VAlUES ('${marketTick.time}', '${marketTick.name}', '${marketTick.value}')`,
-        (err, res) => {
-            if (err) throw err;
-
-            if (typeof cb === "function")
-                cb(marketTick)
-        })
-}
+dbi.RetrieveHistorical("ADA-EUR")
